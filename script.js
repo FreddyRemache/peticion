d2465fcd625d85813e93436744c4714490dc91eb
FreddyRemache <50203650+FreddyRemache@users.noreply.github.com>
2021-10-20 01:49:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let si=document.getElementById('si');
+si.addEventListener("click",function(e){
+  alert("Sabía que aceptarías");
+});
+
+let no=document.getElementById('no');
+no.addEventListener("mousemove",function(e){
+  let x = Math.round(Math.random()*90);
+  let y = Math.round(Math.random()*90);
+  no.style.left = x+"%";
+  no.style.top = y+"%";
+});
